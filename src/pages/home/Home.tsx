2bb1df6f07465916d273691a6517422f333b6e03
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-05 13:49:21 +0800

    Add files via upload

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,8 @@
 
 import "./Home.scss";
 import TopBox from "../../components/TopBox/TopBox.tsx";
+import ChartBox from "../../components/chartBox/ChartBox.tsx";
+import {chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser} from "../../data.ts";
 const Home =()=>{
 
     return(
@@ -8,14 +10,14 @@ const Home =()=>{
             <div className={"box box1"}>
                 <TopBox/>
             </div>
-            <div className={"box box2"}>box2</div>
-            <div className={"box box3"}>box3</div>
-            <div className={"box box4"}>box4</div>
-            <div className={"box box5"}>box5</div>
-            <div className={"box box6"}>box6</div>
+            <div className={"box box2"}><ChartBox {...chartBoxUser}/></div>
+            <div className={"box box3"}><ChartBox {...chartBoxProduct}/></div>
+            <div className={"box box4"}><ChartBox {...chartBoxRevenue}/></div>
+            <div className={"box box5"}><ChartBox {...chartBoxConversion}/></div>
+            <div className={"box box6"}>6</div>
             <div className={"box box7"}>box7</div>
             <div className={"box box8"}>box8</div>
-            <div className={"box box9"}>box1</div>
+            <div className={"box box9"}>box9</div>
         </div>
     )
 }
