87c7e6612c4c97be8a797ec16ede06714d9e6a8a
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-09 17:47:56 +0800

    Add files via upload

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,13 +1,50 @@
 
 import "./test.scss";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+
 const Test =()=>{
 
     return(
         <div className={"test"}>
-            testtest:D
-            {/*<div data-aos="fade-up-left">*/}
-            {/*    testtest:D*/}
-            {/*</div>*/}
+            Test animation
+
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <div className={"h-48"}>
+                        <img className={"w-72 object-center"} src={"./pic1.jpg"} alt={"pic1"}/>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={"h-48 overflow-y-clip"}>
+                        <img className={"w-72 object-center"} src={"./pic2.jpg"} alt={"pic2"}/>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={"h-48 overflow-y-clip"}>
+                        <img className={"w-72 object-center"} src={"./pic3.jpg"} alt={"pic3"}/>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={"h-48 overflow-y-clip"}>
+                        <img className={"w-72 object-contain"} src={"./pic4.jpg"} alt={"pic4"}/>
+                    </div>
+                </SwiperSlide>
+                ...
+            </Swiper>
+
+            <div>
+                <img className={"hover:-rotate-6 w-[38%] border-8 border-amber-300"} src={"./pic4.jpg"} alt={"pic4"}/>
+
+            </div>
         </div>
     )
 }
