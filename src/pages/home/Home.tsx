d1fa5ac5d6bcc2ffc903d9ba9dcb4a667b97ed31
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-09 15:17:11 +0800

    Add files via upload

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,7 +11,8 @@ import {
     chartBoxUser
 } from "../../data.ts";
 import BarChartBox from "../../components/barChartBox/BarChartBox.tsx";
-import PieChartBox from "../../components/pieChartBox/pieChartBox.tsx";
+import PieChartBox from "../../components/pieChartBox/PieChartBox.tsx";
+import BigChartBox from "../../components/bigChartBox/BigChartBox.tsx";
 const Home =()=>{
 
     return(
@@ -21,10 +22,10 @@ const Home =()=>{
             </div>
             <div className={"box box2"}><ChartBox {...chartBoxUser}/></div>
             <div className={"box box3"}><ChartBox {...chartBoxProduct}/></div>
-            <div className={"box box4"}>\<PieChartBox/></div>
+            <div className={"box box4"}><PieChartBox/></div>
             <div className={"box box5"}><ChartBox {...chartBoxConversion}/></div>
             <div className={"box box6"}><ChartBox {...chartBoxRevenue}/></div>
-            <div className={"box box7"}>box7</div>
+            <div className={"box box7"}><BigChartBox /></div>
             <div className={"box box8"}><BarChartBox {...barChartBoxVisit}/></div>
             <div className={"box box9"}><BarChartBox {...barChartBoxRevenue}/></div>
         </div>
