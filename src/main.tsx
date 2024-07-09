6b4acd3cc0c907d321520f2d99eaa3e510eeb748
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-09 17:46:51 +0800

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
