49c1e5623fecf7af159dc2d9585c69e9b6d07cca
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-26 18:39:28 +0800

    Add files via upload

--- src/pages/boardthree/BoardThree.tsx
+++ src/pages/boardthree/BoardThree.tsx
@@ -1,40 +1,78 @@
 import React, {useState} from "react";
 import './boardthree.scss';
+
 const BoardThree = () => {
     const [widgets, setWidgets] = useState<string[]>([])
 
-    function handleOnDrag(e:React.DragEvent,widgetType:string){
+    function handleOnDrag(e: React.DragEvent, widgetType: string) {
         e.dataTransfer.setData("widgetType", widgetType);
     }
-    function handleOnDrop(e:React.DragEvent){
-        const widgetType =e.dataTransfer.getData("widgetType");
+
+    function handleOnDrop(e: React.DragEvent) {
+        const widgetType = e.dataTransfer.getData("widgetType");
         console.log("widgetType", widgetType);
         setWidgets([...widgets, widgetType]);
     }
-    function handleDragOver(e:React.DragEvent){
+
+    function handleDragOver(e: React.DragEvent) {
         e.preventDefault();
         console.log("drag over");
     }
+
+    // const editor = new EditorJS({
+    //     /**
+    //      * Id of Element that should contain the Editor
+    //      */
+    //     holder: 'editorjs',
+    //
+    //     /**
+    //      * Available Tools list.
+    //      * Pass Tool's class or Settings object for each Tool you want to use
+    //      */
+    //     tools: {
+    //         header: {
+    //             class: Header,
+    //             inlineToolbar: ['link']
+    //         },
+    //         list: {
+    //             class: List,
+    //             inlineToolbar: true
+    //         }
+    //     },
+    // })
+    // <p>{JSON.stringify(data, null, 4)}</p>
+    // <p>{JSON.stringify(getValues(), null, 4)}</p>
     return (
         <>
-            <div className={"widgets"}>
-                <div
-                    className={"widget"}
-                    draggable
-                    onDragStart={(e) => handleOnDrag(e, "WidgetA")}>
-                    Widget A
+            <div>
+                <button
+                    className={"p-2 bg-black text-white"}
+                    onClick={() => {
+                        console.log(widgets)
+                    }}>Export
+                </button>
+                <div className={"widgets"}>
+                    <div
+                        className={"widget"}
+                        draggable
+                        onDragStart={(e) => handleOnDrag(e, "WidgetA")}>
+                        Widget A
+                    </div>
+                    <div
+                        className={"widget"}
+                        draggable
+                        onDragStart={(e) => handleOnDrag(e, "WidgetB")}>
+                        Widget B
+                    </div>
                 </div>
-                <div
-                    className={"widget"}
-                    draggable
-                    onDragStart={(e) => handleOnDrag(e, "WidgetB")}>
-                    Widget B
+                <div className={"page"} onDrop={handleOnDrop} onDragOver={handleDragOver}>
+                    {widgets.map((widget, index) => (
+                        <div className={"dropped-widget"} key={index}>{widget}</div>
+                    ))}
                 </div>
             </div>
-            <div className={"page"} onDrop={handleOnDrop} onDragOver={handleDragOver}>
-                {widgets.map((widget, index)=> (
-                    <div className={"dropped-widget"} key={index}>{widget}</div>
-                ))}
+            <div>
+                {/*editorjs*/}
             </div>
         </>
     )
